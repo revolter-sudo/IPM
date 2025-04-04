51fe2820b445bbf236e87d34c5c50577ea70ce5f
yash.bhatt <yash.bhatt@nextgenclearing.com>
2025-04-04 12:39:24 +0530

    ragister user fix

--- src/app/database/models.py
+++ src/app/database/models.py
@@ -96,7 +96,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     uuid = Column(UUID(as_uuid=True), unique=True, nullable=False, default=uuid.uuid4)
     name = Column(String(255), nullable=False)
-    phone = Column(BigInteger, unique=True, nullable=False)
+    phone = Column(BigInteger, unique=False, nullable=False)
     password_hash = Column(String(255), nullable=False)
     role = Column(String(30), nullable=False)
     is_deleted = Column(Boolean, nullable=False, default=False)
